4bfc30db2b414b750a4da455af9ea9e146b4f8aa
student <a@example.com>
2026-03-04 18:24:04 +0100

    Zadanie 5

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-    Segment seg = new Segment();
+    Segment s1 = new Segment();
 
-    seg.p1 = new Point();
-    seg.p1.x = 0;
-    seg.p1.y = 0;
-    seg.p2 = new Point();
-    seg.p2.x = 3;
-    seg.p2.y = 4;
-        System.out.println("Dlugosc: " +seg.length());
+    s1.p1 = new Point();
+    s1.p1.x = 0;
+    s1.p1.y = 0;
+    s1.p2 = new Point();
+    s1.p2.x = 3;
+    s1.p2.y = 4;
+    Segment s2 = new Segment();
+    s2.p1 = new Point();
+    s2.p1.x = 0;
+    s2.p1.y = 0;
+    s2.p2 = new Point();
+    s2.p2.x = 10;
+    s2.p2.y = 10;
+    Segment[] tablica = {s1, s2};
+    Segment najdluzszy = Segment.longestSegment(tablica);
+        System.out.println("Najdluzszy ma dlugosc: " + najdluzszy.length());
     }
 }
--- src/Segment.java
+++ src/Segment.java
@@ -7,4 +7,13 @@ public class Segment{
         double dy = p2.y - p1.y;
         return Math.sqrt(dx * dx + dy * dy);
     }
+    public static Segment longestSegment(Segment[] arr){
+        Segment max = arr[0];
+        for(int i =1; i < arr.length; i++){
+            if(arr[i].length() > max.length()){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
 }
