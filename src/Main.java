611e3dfd369b001377d255351d2fa134fdebec1e
student <a@example.com>
2026-03-04 18:40:17 +0100

    Zadanie 1 lab 2

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-    Segment s1 = new Segment();
-
-    s1.p1 = new Point();
-    s1.p1.x = 0;
-    s1.p1.y = 0;
-    s1.p2 = new Point();
-    s1.p2.x = 3;
-    s1.p2.y = 4;
-    Segment s2 = new Segment();
-    s2.p1 = new Point();
-    s2.p1.x = 0;
-    s2.p1.y = 0;
-    s2.p2 = new Point();
-    s2.p2.x = 10;
-    s2.p2.y = 10;
-    Segment[] tablica = {s1, s2};
-    Segment najdluzszy = Segment.longestSegment(tablica);
-        System.out.println("Najdluzszy ma dlugosc: " + najdluzszy.length());
+    Point p = new Point(5.5, 10.0);
+        System.out.println("Prywatne pole x pobrane getterem: " + p.getX());
+        Point pZio = new Point();
+        System.out.println("Punkt domyślny: " +pZio.toString());
     }
 }
--- src/Point.java
+++ src/Point.java
@@ -1,6 +1,27 @@
 public class Point{
-    public double x;
-    public double y;
+    private double x;
+    private double y;
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    public Point(){
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public double getX(){
+        return x;
+    }
+    public void setX(double x){
+        this.x = x;
+    }
+    public double getY(){
+        return y;
+    }
+    public void setY(double y){
+        this.y = y;
+    }
     public String toString(){
         return "Punkt: (" + x + ", " + y + ")";
     }
