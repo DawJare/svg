04dc67bc8ca4de5ee6e182abd86d1e0fefbe2360
student <a@example.com>
2026-03-25 19:18:41 +0100

    Zadanie 7

--- genealogy/src/Family.java
+++ genealogy/src/Family.java
@@ -4,8 +4,10 @@ import java.util.Map;
 public class Family{
     private Map<String, Person> members = new HashMap<>();
 
-    public void add(Person person){
-        members.put(person.getFullName(), person);
+    public void add(Person... persons){
+        for (Person p : persons) {
+            members.put(p.getFullName(), p);
+        }
     }
     public Person get(String fullName){
         return members.get(fullName);
