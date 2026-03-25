88d8f0da390ba5cf63ff598d44d21eb4b9028b15
student <a@example.com>
2026-03-25 18:37:23 +0100

    Zadanie 2

--- genealogy/src/Person.java
+++ genealogy/src/Person.java
@@ -1,14 +1,21 @@
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Person{
     private String firstName;
     private String lastName;
     private LocalDate birthDate;
 
+    private Set<Person> children = new HashSet<>();
+
     public Person(String firstName, String lastName, LocalDate birthDate){
         this.firstName = firstName;
         this.lastName = lastName;
         this.birthDate = birthDate;
+    }
+    public boolean adopt(Person child){
+        return children.add(child);
     }
     public String getFullName(){
         return firstName + " " + lastName;
